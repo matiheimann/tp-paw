4261452620d4c009f05ea5995654af1009276377
Diego Bruno <dbruno@itba.edu.ar>
2018-09-28 11:53:08 -0300

    Added transaction manager

--- pawddit/webapp/src/main/java/ar/edu/itba/pawddit/webapp/config/WebConfig.java
+++ pawddit/webapp/src/main/java/ar/edu/itba/pawddit/webapp/config/WebConfig.java
@@ -11,10 +11,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -22,11 +25,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+@Configuration
 @EnableWebMvc
+@EnableTransactionManagement
 @ComponentScan({ "ar.edu.itba.pawddit.webapp.controller", 
 				 "ar.edu.itba.pawddit.services", 
 	             "ar.edu.itba.pawddit.persistence", })
-@Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 	
 	@Value("classpath:schema.sql")
@@ -78,6 +82,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		messageSource.setDefaultEncoding(StandardCharsets.UTF_8.displayName());
 		messageSource.setCacheSeconds(5);
 		return messageSource;
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(final DataSource ds) {
+		return new DataSourceTransactionManager(ds);
 	}
 
 }

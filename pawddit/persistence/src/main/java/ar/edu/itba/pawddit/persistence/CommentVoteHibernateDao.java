5ff61cfcb195d6b19214edaddc83b999f798dc8a
matiheimann <mheimann@itba.edu.ar>
2018-11-10 01:27:35 -0300

    Vote comment Dao and Service

---
+++ pawddit/persistence/src/main/java/ar/edu/itba/pawddit/persistence/CommentVoteHibernateDao.java
@@ -0,0 +1,59 @@
+package ar.edu.itba.pawddit.persistence;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+
+import ar.edu.itba.pawddit.model.Comment;
+import ar.edu.itba.pawddit.model.User;
+import ar.edu.itba.pawddit.model.VoteComment;
+
+@Repository
+public class CommentVoteHibernateDao implements CommentVoteDao{
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public VoteComment voteComment(User user, Comment comment, Integer value) {
+		final VoteComment voteComment = new VoteComment(user, comment, value);
+		em.persist(voteComment);
+		return voteComment;
+	}
+
+	@Override
+	public void changeVote(User user, Comment comment) {
+		final VoteComment vc = find(user, comment).get();
+		vc.setValue(vc.getValue() * -1);
+		
+	}
+
+	@Override
+	public void cancelVote(User user, Comment comment) {
+		final VoteComment vc = find(user, comment).get();
+		em.remove(vc);
+	}
+
+	@Override
+	public int checkVote(User user, Comment comment) {
+		final Optional<VoteComment> vc = find(user, comment);
+		if(vc.isPresent())
+		{
+			return 1;
+		}
+		return 0;
+	}
+	
+	private Optional<VoteComment> find(final User user, final Comment comment) {
+		final TypedQuery<VoteComment> query = em.createQuery("from VoteComment as vc where vc.pk.user = :user and vc.pk.comment = :comment", VoteComment.class);
+		query.setParameter("user", user);
+		query.setParameter("comment", comment);
+		return query.getResultList().stream().findFirst();
+	}
+	
+
+}
---
+++ pawddit/services/src/main/java/ar/edu/itba/pawddit/services/CommentVoteServiceImpl.java
@@ -0,0 +1,58 @@
+package ar.edu.itba.pawddit.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ar.edu.itba.pawddit.model.Comment;
+import ar.edu.itba.pawddit.model.User;
+import ar.edu.itba.pawddit.persistence.CommentVoteDao;
+
+public class CommentVoteServiceImpl implements CommentVoteService{
+	
+	@Autowired
+	CommentVoteDao cvd;
+
+	@Override
+	public void upVote(User user, Comment comment) {
+		
+		final Integer vote = cvd.checkVote(user, comment);
+		
+		if(vote == 1)
+		{
+			cvd.cancelVote(user, comment);
+		}
+		else if(vote == -1)
+		{
+			cvd.changeVote(user, comment);
+		}
+		else
+		{
+			cvd.voteComment(user, comment, 1);
+		}
+		
+	}
+
+	@Override
+	public void downVote(User user, Comment comment) {
+		
+		final Integer vote = cvd.checkVote(user, comment);
+		
+		if(vote == -1){
+			cvd.cancelVote(user, comment);
+		}
+		else if(vote == 1){
+			cvd.changeVote(user, comment);
+		}
+		else{
+			cvd.voteComment(user, comment, -1);
+		}
+		
+	}
+
+	@Override
+	public int checkVote(User user, Comment comment) {
+		return cvd.checkVote(user, comment);
+	}
+
+
+
+}

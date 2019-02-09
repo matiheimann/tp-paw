dd4dc82f90eb0d983da709542425ebbb33993a70
matiheimann <mheimann@itba.edu.ar>
2019-02-09 05:57:03 -0300

    Registration passwords match

--- front-generator/app/scripts/controllers/RegisterModalCtrl.js
+++ front-generator/app/scripts/controllers/RegisterModalCtrl.js
@@ -2,14 +2,21 @@
 define(['pawddit', 'services/restService'], function(pawddit) {
 
 	pawddit.controller('RegisterModalCtrl', ['$scope', '$location', '$uibModalInstance', 'restService', function($scope, $location, $modal, restService) {
-		$scope.newGroup = {};
+		$scope.newUser = {};
 
 		$scope.cancel = function() {
 			$scope.$dismiss();
 		};
 
 		$scope.doSubmit = function() {
-	
+			$scope.emailMatch = false;
+			$scope.usernameUsed = false;
+			$scope.passwordNotMatch = false;
+
+			if($scope.newUser.password != $scope.newUser.confirmPassword){
+				$scope.passwordNotMatch = true;
+			}
+
 		};
 
 	}]);

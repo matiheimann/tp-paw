90f7b512d68049cbd68a9166d15b74de2c43515d
Diego Bruno <dbruno@itba.edu.ar>
2019-02-15 11:08:41 -0300

    Fixed delete buttons on logout

--- front-generator/app/scripts/controllers/IndexCtrl.js
+++ front-generator/app/scripts/controllers/IndexCtrl.js
@@ -29,6 +29,8 @@ define(['pawddit', 'jquery', 'services/restService', 'services/modalService', 's
 				$scope.isLoggedIn = data.isLoggedIn;
         		if ($scope.isLoggedIn) {
 					getLoggedUser();
+				} else {
+					$scope.loggedUser = null;
 				}
 			});
 		});

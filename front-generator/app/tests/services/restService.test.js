684c8836f6c2127d34ada24942cf2fd754a7fc93
matiheimann <mheimann@itba.edu.ar>
2019-02-15 15:17:30 -0300

    Rest service test

--- front-generator/app/tests/services/restService.test.js
+++ front-generator/app/tests/services/restService.test.js
@@ -1,7 +1,14 @@
 define(['services/restService', 'angular-mocks'], function() {
 
     describe('Rest Service', function() {
+
     	var restService, url, $httpBackend, $q;
+        var MOCK_GROUP = {name: 'group', description: 'the best group'};
+        var MOCK_POSTS = [{postid: '1', title: 'My first post', groupname: 'group', content:'My content', userid: '1'},
+                          {postid: '2', title: 'My second post', groupname: 'group', content: 'My content', userid: '1'},
+                          {postid: '3', title: 'My third post', groupname: 'group', content: 'Lorem ipsum', userid: '1'}];
+
+        var RESPONSE_ERROR = {detail: 'Not found.'};
 
     	beforeEach(module('pawddit'));
 
@@ -14,6 +21,66 @@ define(['services/restService', 'angular-mocks'], function() {
 
         it('should be defined', function() {
             expect(restService).toBeDefined();
+        });
+
+        describe('.getGroup()', function(){
+
+            var response;
+
+            it('should be define', function() {
+                expect(restService.getUser).toBeDefined();
+            });
+
+            it('should return a valid group given a groupname', function() {
+
+                $httpBackend.whenGET(url + '/groups/' + MOCK_GROUP.name).respond(200, $q.when(MOCK_USER));
+
+                restService.getGroup(MOCK_GROUP.name)
+                .then(function(res) {
+                    response = res;
+                });
+
+                $httpBackend.flush();
+
+                expect(response).toEqual(MOCK_GROUP);
+            });
+
+            it('should return with a 404 when called with an non-existent groupname', function() {
+
+                $httpBackend.whenGET(url + '/groups/' + MOCK_GROUP.name).respond(404, RESPONSE_ERROR);
+
+                restService.getUser(MOCK_GROUP.name)
+                .catch(function(res) {
+                    response = res.data;
+                });
+
+                $httpBackend.flush(); 
+
+                expect(response).toEqual(MOCK_GROUP);
+            });
+
+        });
+
+        describe('.getPosts()', function() {
+
+            var response;
+
+            it('should be defined', function() {
+                expect(restService.getPosts).toBeDefined();
+            });
+           
+            it('should return a valid list of posts given no parameters', function() {
+                $httpBackend.whenGET(url + '/posts').respond(200, $q.when(MOCK_POSTS));
+
+                restService.getProducts()
+                .then(function(res) {
+                    response = res;
+                });
+
+                $httpBackend.flush();
+
+                expect(response).toEqual(MOCK_POSTS);
+            });
         });
     });
 });

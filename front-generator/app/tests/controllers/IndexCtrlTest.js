3ec43ea3edb358a41bfd3e29480c8e0e6e95163e
matiheimann <mheimann@itba.edu.ar>
2019-02-15 16:16:09 -0300

    Index Controller Test

---
+++ front-generator/app/tests/controllers/IndexCtrlTest.js
@@ -0,0 +1,21 @@
+define(['controllers/IndexCtrl', 'angular-mocks'], function() {
+
+	describe('Index Controller', function() {
+		
+		var controller, scope;
+		
+		beforeEach(module('pawditt'));
+		
+		beforeEach(inject(function($injector, $rootScope, $controller) {
+			scope = $rootScope.$new();
+			controller = $controller('IndexCtrl', { $scope: scope });
+		}));
+		
+		describe('$scope.isLoggedIn', function() {
+			it('should be false', function() {
+				expect(scope.isLoggedIn).toBe(false);
+			});
+			
+		});
+	});
+});

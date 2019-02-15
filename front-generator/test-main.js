e8dfc37ab8a297567f944b03e0a27008c270d64b
jkatan <jkatan@users.noreply.github.com>
2019-02-15 03:13:55 -0300

    update test-main file

--- front-generator/test-main.js
+++ front-generator/test-main.js
@@ -1,16 +1,11 @@
-var allTestFiles = []
-var TEST_REGEXP = /(spec|test)\.js$/i
-
-// Get a list of all the test files to include
-Object.keys(window.__karma__.files).forEach(function (file) {
-  if (TEST_REGEXP.test(file)) {
-    // Normalize paths to RequireJS module names.
-    // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
-    // then do not normalize the paths
-    var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '')
-    allTestFiles.push('../../' + normalizedTestModule)
+var tests = [];
+for (var file in window.__karma__.files) {
+  if (window.__karma__.files.hasOwnProperty(file)) {
+    if (/Spec\.js$/.test(file)) {
+      tests.push(file);
+    }
   }
-})
+}
 
 require.config({
     // Karma serves files under /base, which is the basePath from your config file
@@ -122,9 +117,10 @@ require.config({
     packages: [
 
     ],
-    // dynamically load all test files
-    deps: allTestFiles,
 
-    // we have to kickoff jasmine, as it is asynchronous
+    // ask Require.js to load these files (all our tests)
+    deps: tests,
+
+    // start test run, once Require.js is done
     callback: window.__karma__.start
 })
